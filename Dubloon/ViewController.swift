d634305350d1e3030d6f93aed0ce3b3eb7dd5789
Tyler Aguilar <tyleraguilar93@gmail.com>
2021-11-24 11:14:35 -0500

    Initial Commit

---
+++ Dubloon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dubloon
+//
+//  Created by Tyler Aguilar on 11/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DubloonUITests/DubloonUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DubloonUITestsLaunchTests.swift
+//  DubloonUITests
+//
+//  Created by Tyler Aguilar on 11/24/21.
+//
+
+import XCTest
+
+class DubloonUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
